3ce2084d6dae14d0a162c6115942368ed9fc5442
Maurice Devs <166598671+mouricedevs@users.noreply.github.com>
2024-08-23 20:10:08 +0300

    Gifted

---
+++ gifted/cmds/ping.js
@@ -0,0 +1,67 @@
+(function (_0x3a8a07, _0x41a29a) {
+    const _0x7f50ef = _0x36e3, _0x650613 = _0x3a8a07();
+    while (!![]) {
+        try {
+            const _0x506b3c = -parseInt(_0x7f50ef(0xd4)) / 0x1 + -parseInt(_0x7f50ef(0xd8)) / 0x2 * (parseInt(_0x7f50ef(0xcf)) / 0x3) + parseInt(_0x7f50ef(0xe9)) / 0x4 * (-parseInt(_0x7f50ef(0xd9)) / 0x5) + -parseInt(_0x7f50ef(0xe5)) / 0x6 * (-parseInt(_0x7f50ef(0xda)) / 0x7) + parseInt(_0x7f50ef(0xe6)) / 0x8 * (parseInt(_0x7f50ef(0xe0)) / 0x9) + -parseInt(_0x7f50ef(0xde)) / 0xa * (-parseInt(_0x7f50ef(0xd6)) / 0xb) + parseInt(_0x7f50ef(0xd2)) / 0xc * (parseInt(_0x7f50ef(0xe8)) / 0xd);
+            if (_0x506b3c === _0x41a29a)
+                break;
+            else
+                _0x650613['push'](_0x650613['shift']());
+        } catch (_0x5e7c38) {
+            _0x650613['push'](_0x650613['shift']());
+        }
+    }
+}(_0x4cc1, 0x866ea));
+const ping = async (_0x1a08d2, _0x4dd90b) => {
+    const _0x650468 = _0x36e3, _0x4b24a4 = /^[\\/!#.]/gi['test'](_0x1a08d2[_0x650468(0xe3)]) ? _0x1a08d2[_0x650468(0xe3)][_0x650468(0xdc)](/^[\\/!#.]/gi)[0x0] : '/', _0x416e49 = _0x1a08d2['body'][_0x650468(0xdb)](_0x4b24a4) ? _0x1a08d2[_0x650468(0xe3)][_0x650468(0xd7)](_0x4b24a4[_0x650468(0xe4)])[_0x650468(0xd5)]() : '';
+    if (_0x416e49 === _0x650468(0xe2)) {
+        const _0xf22616 = new Date(), {key: _0x27ffcc} = await _0x4dd90b[_0x650468(0xe7)](_0x1a08d2[_0x650468(0xdd)], { 'text': _0x650468(0xdf) }, { 'quoted': _0x1a08d2 });
+        await _0x1a08d2[_0x650468(0xd1)]('🚀');
+        const _0x242256 = _0x650468(0xd0) + (new Date() - _0xf22616) + _0x650468(0xe1);
+        return await _0x1a08d2[_0x650468(0xd1)]('⚡'), _0x1a08d2[_0x650468(0xd3)]('' + _0x242256, { 'quoted': _0x27ffcc });
+    }
+};
+function _0x36e3(_0x263db1, _0x5c5f9c) {
+    const _0x4cc15c = _0x4cc1();
+    return _0x36e3 = function (_0x36e3cb, _0x5af39a) {
+        _0x36e3cb = _0x36e3cb - 0xcf;
+        let _0x1bd492 = _0x4cc15c[_0x36e3cb];
+        return _0x1bd492;
+    }, _0x36e3(_0x263db1, _0x5c5f9c);
+}
+function _0x4cc1() {
+    const _0x2c867b = [
+        'sendMessage',
+        '5785VvvEsq',
+        '4AMmYzF',
+        '834597YbmIVx',
+        '*_ɢɪғᴛᴇᴅ-ᴍᴅ\x20sᴘᴇᴇᴅ:\x20',
+        'React',
+        '23748omVzBI',
+        'reply',
+        '839267yQbRdL',
+        'toLowerCase',
+        '11FPQLIA',
+        'slice',
+        '6lvRdnm',
+        '3073820XnknlK',
+        '5208791pRZKsY',
+        'startsWith',
+        'match',
+        'from',
+        '6172620fAjbyz',
+        '*_ᴘɪɴɢɪɴɢ\x20ᴛᴏ\x20sᴇʀᴠᴇʀ..._*',
+        '8244GeQSZU',
+        '\x20ms_*',
+        'ping',
+        'body',
+        'length',
+        '6GUXYCk',
+        '5216SYjkqW'
+    ];
+    _0x4cc1 = function () {
+        return _0x2c867b;
+    };
+    return _0x4cc1();
+}
+export default ping;
