6dadf86e6307d2399822ab74694bb46f8bb2c8c4
Gifted Maurice <166598671+mouricedevs@users.noreply.github.com>
2024-09-30 21:41:55 +0300

    Gifted

--- gifted/cmds/ebase.js
+++ gifted/cmds/ebase.js
@@ -1,105 +1,118 @@
-(function (_0x450269, _0x30c5ac) {
-    const _0x495da0 = _0x51d5, _0x12dcaf = _0x450269();
+// GIFTED-MD //
+
+function gifted_0x3d43(_0x5810ee, _0x22b86d) {
+    const _0xd1079a = gifted_0xd107();
+    return gifted_0x3d43 = function (_0x3d4340, _0x4481f7) {
+        _0x3d4340 = _0x3d4340 - 0x67;
+        let _0x451c02 = _0xd1079a[_0x3d4340];
+        return _0x451c02;
+    }, gifted_0x3d43(_0x5810ee, _0x22b86d);
+}
+(function (_0x4b5182, _0x213cb5) {
+    const _0x333916 = gifted_0x3d43, _0x1d8d8f = _0x4b5182();
     while (!![]) {
         try {
-            const _0x22e21c = -parseInt(_0x495da0(0x177)) / 0x1 * (-parseInt(_0x495da0(0x173)) / 0x2) + parseInt(_0x495da0(0x163)) / 0x3 + -parseInt(_0x495da0(0x16c)) / 0x4 * (-parseInt(_0x495da0(0x171)) / 0x5) + parseInt(_0x495da0(0x169)) / 0x6 * (-parseInt(_0x495da0(0x15d)) / 0x7) + parseInt(_0x495da0(0x16a)) / 0x8 * (-parseInt(_0x495da0(0x167)) / 0x9) + parseInt(_0x495da0(0x17b)) / 0xa + parseInt(_0x495da0(0x165)) / 0xb;
-            if (_0x22e21c === _0x30c5ac)
+            const _0x1fe948 = -parseInt(_0x333916(0x6b)) / 0x1 * (-parseInt(_0x333916(0x6e)) / 0x2) + -parseInt(_0x333916(0x79)) / 0x3 * (-parseInt(_0x333916(0x90)) / 0x4) + -parseInt(_0x333916(0x88)) / 0x5 * (parseInt(_0x333916(0x92)) / 0x6) + -parseInt(_0x333916(0x7c)) / 0x7 * (-parseInt(_0x333916(0x77)) / 0x8) + -parseInt(_0x333916(0x93)) / 0x9 + parseInt(_0x333916(0x6f)) / 0xa + parseInt(_0x333916(0x85)) / 0xb;
+            if (_0x1fe948 === _0x213cb5)
                 break;
             else
-                _0x12dcaf['push'](_0x12dcaf['shift']());
-        } catch (_0x32a4d2) {
-            _0x12dcaf['push'](_0x12dcaf['shift']());
+                _0x1d8d8f['push'](_0x1d8d8f['shift']());
+        } catch (_0x5b2dc1) {
+            _0x1d8d8f['push'](_0x1d8d8f['shift']());
         }
     }
-}(_0x518f, 0x30c11));
-function _0x518f() {
-    const _0x440a93 = [
-        'create',
-        '88828ttVSWk',
-        'pushName',
-        'https://api.maskser.me/api/tools/ebase64?text=',
-        'ebase',
-        'result',
-        '5TvybyW',
+}(gifted_0xd107, 0xddcbc));
+function gifted_0xd107() {
+    const _0xb0f733 = [
+        'slice',
+        'Footer',
+        'toLowerCase',
+        'Error\x20getting\x20GPT\x20response:',
+        'cta_copy',
+        '8MUrLQr',
+        'trim',
+        '14295TxKWmM',
+        'error',
+        'Error\x20getting\x20response\x20from\x20GPT.',
+        '3101224VJZDnP',
+        'reply',
         'Message',
-        '478SKOUIv',
+        'Header',
+        'data',
+        'NativeFlowMessage',
+        '&apikey=',
         'length',
+        'startsWith',
+        '1591249isywkx',
+        'relayMessage',
+        'result',
+        '65mfnOpC',
+        'key',
+        'copy_code',
+        'sendMessage',
+        'Copy\x20Your\x20Code',
+        'includes',
+        '_,*\x0aPlease\x20Provide\x20Me\x20a\x20text\x20to\x20Encrypt.',
+        'body',
+        '44PapJMk',
+        'React',
+        '682314EGPBaa',
+        '9123759CErjPy',
         'InteractiveMessage',
         'from',
-        '151eqXFCy',
-        'Invalid\x20response\x20from\x20the\x20GPT\x20API.',
-        'reply',
-        'get',
-        '102200abVfRL',
-        'data',
         'Hello\x20*_',
-        'remoteJid',
-        'A\x20moment,...',
-        '_,*\x0aPlease\x20Provide\x20Me\x20a\x20text\x20to\x20Encrypt.',
-        'toLowerCase',
-        'match',
-        'relayMessage',
-        'sendMessage',
-        'split',
-        'Error\x20getting\x20response\x20from\x20GPT.',
-        'Header',
-        'Body',
-        '2516073PJRCwV',
-        'cta_copy',
-        'body',
-        'React',
-        'error',
-        'slice',
-        '531501imlFJI',
         'message',
-        '4048891somdob',
-        'key',
-        '252TpyqPz',
-        'Error\x20getting\x20GPT\x20response:',
-        '6prjbJJ',
-        '15608NDrzSg'
+        'Invalid\x20response\x20from\x20the\x20GPT\x20API.',
+        '7727KQdHQV',
+        'pushName',
+        'match',
+        '342YMMoeJ',
+        '14391510TPLrzi',
+        'A\x20moment...',
+        'create'
     ];
-    _0x518f = function () {
-        return _0x440a93;
+    gifted_0xd107 = function () {
+        return _0xb0f733;
     };
-    return _0x518f();
+    return gifted_0xd107();
 }
-import _0x45cd31 from 'axios';
-import _0x5539b5, { prepareWAMessageMedia } from 'gifted-baileys';
-const {generateWAMessageFromContent, proto} = _0x5539b5, Ebase = async (_0x1da64c, _0x179c56) => {
-        const _0x56177d = _0x51d5, _0x153d65 = _0x1da64c['body']['match'](/^[\\/!#.]/), _0x22a676 = _0x153d65 ? _0x153d65[0x0] : '/', _0x4e28b4 = _0x1da64c['body']['startsWith'](_0x22a676) ? _0x1da64c[_0x56177d(0x15f)][_0x56177d(0x162)](_0x22a676[_0x56177d(0x174)])[_0x56177d(0x185)]('\x20')[0x0][_0x56177d(0x181)]() : '', _0x30b4b1 = _0x1da64c['body'][_0x56177d(0x162)](_0x22a676[_0x56177d(0x174)] + _0x4e28b4['length'])['trim'](), _0x426b4c = [_0x56177d(0x16f)];
-        if (_0x426b4c['includes'](_0x4e28b4)) {
-            if (!_0x30b4b1)
-                return _0x1da64c[_0x56177d(0x179)](_0x56177d(0x17d) + _0x1da64c[_0x56177d(0x16d)] + _0x56177d(0x180));
+import gifted_0x55d584 from 'axios';
+import gifted_0x32c317, { prepareWAMessageMedia } from 'gifted-baileys';
+const {generateWAMessageFromContent, proto} = gifted_0x32c317, Ebase = async (_0x51b4db, _0x481271) => {
+        const _0x633706 = gifted_0x3d43, _0x22fdfb = _0x51b4db['body'][_0x633706(0x6d)](/^[\\/!#.]/), _0x4be40b = _0x22fdfb ? _0x22fdfb[0x0] : '/', _0x1c97f = 'https://giftedapis.us.kg', _0x4dc476 = '_0x5aff35,_0x187643', _0x282487 = _0x51b4db[_0x633706(0x8f)][_0x633706(0x84)](_0x4be40b) ? _0x51b4db['body'][_0x633706(0x72)](_0x4be40b[_0x633706(0x83)])['split']('\x20')[0x0][_0x633706(0x74)]() : '', _0x37d765 = _0x51b4db[_0x633706(0x8f)][_0x633706(0x72)](_0x4be40b[_0x633706(0x83)] + _0x282487[_0x633706(0x83)])[_0x633706(0x78)](), _0x6e50cd = ['ebase'];
+        if (_0x6e50cd[_0x633706(0x8d)](_0x282487)) {
+            if (!_0x37d765)
+                return _0x51b4db[_0x633706(0x7d)](_0x633706(0x68) + _0x51b4db[_0x633706(0x6c)] + _0x633706(0x8e));
             try {
-                await _0x1da64c[_0x56177d(0x160)]('🕘'), await _0x1da64c[_0x56177d(0x179)](_0x56177d(0x17f));
-                const _0x511274 = _0x56177d(0x16e) + encodeURIComponent(_0x30b4b1), _0x392267 = await _0x45cd31[_0x56177d(0x17a)](_0x511274), _0x27152d = _0x392267[_0x56177d(0x17c)];
-                if (_0x27152d && _0x27152d[_0x56177d(0x170)]) {
-                    const _0x7466ad = _0x27152d[_0x56177d(0x170)], _0x2bc8f8 = _0x7466ad[_0x56177d(0x182)](/```([\s\S]*?)```/);
-                    if (_0x2bc8f8) {
-                        const _0x3d2627 = _0x2bc8f8[0x1];
-                        let _0x4cfccf = generateWAMessageFromContent(_0x1da64c['from'], {
+                await _0x51b4db[_0x633706(0x91)]('🕘'), await _0x51b4db[_0x633706(0x7d)](_0x633706(0x70));
+                let _0x3643b5 = _0x4dc476;
+                const _0x106710 = _0x1c97f + '/api/tools/ebase?query=' + encodeURIComponent(_0x37d765) + _0x633706(0x82) + _0x3643b5, _0x352b8e = await gifted_0x55d584['get'](_0x106710), _0x4d3efe = _0x352b8e[_0x633706(0x80)];
+                if (_0x4d3efe && _0x4d3efe[_0x633706(0x87)]) {
+                    const _0x2a3af2 = _0x4d3efe[_0x633706(0x87)], _0x4d3e94 = _0x2a3af2['match'](/```([\s\S]*?)```/);
+                    if (_0x4d3e94) {
+                        const _0x24fd32 = _0x4d3e94[0x1];
+                        let _0x54651a = generateWAMessageFromContent(_0x51b4db[_0x633706(0x67)], {
                             'viewOnceMessage': {
                                 'message': {
                                     'messageContextInfo': {
                                         'deviceListMetadata': {},
                                         'deviceListMetadataVersion': 0x2
                                     },
-                                    'interactiveMessage': proto[_0x56177d(0x172)][_0x56177d(0x175)][_0x56177d(0x16b)]({
-                                        'body': proto[_0x56177d(0x172)][_0x56177d(0x175)][_0x56177d(0x15c)][_0x56177d(0x16b)]({ 'text': _0x7466ad }),
-                                        'footer': proto[_0x56177d(0x172)]['InteractiveMessage']['Footer'][_0x56177d(0x16b)]({ 'text': '>\x20*©𝟐𝟎𝟐𝟒\x20𝐆𝐈𝐅𝐓𝐄𝐃\x20𝐌𝐃\x20𝐕𝟓*' }),
-                                        'header': proto[_0x56177d(0x172)][_0x56177d(0x175)][_0x56177d(0x187)][_0x56177d(0x16b)]({
+                                    'interactiveMessage': proto[_0x633706(0x7e)][_0x633706(0x94)]['create']({
+                                        'body': proto['Message']['InteractiveMessage']['Body'][_0x633706(0x71)]({ 'text': _0x2a3af2 }),
+                                        'footer': proto[_0x633706(0x7e)][_0x633706(0x94)][_0x633706(0x73)]['create']({ 'text': '>\x20*©𝟐𝟎𝟐𝟒\x20𝐆𝐈𝐅𝐓𝐄𝐃\x20𝐌𝐃\x20𝐕𝟓*' }),
+                                        'header': proto['Message'][_0x633706(0x94)][_0x633706(0x7f)][_0x633706(0x71)]({
                                             'title': '',
                                             'subtitle': '',
                                             'hasMediaAttachment': ![]
                                         }),
-                                        'nativeFlowMessage': proto['Message']['InteractiveMessage']['NativeFlowMessage']['create']({
+                                        'nativeFlowMessage': proto[_0x633706(0x7e)][_0x633706(0x94)][_0x633706(0x81)][_0x633706(0x71)]({
                                             'buttons': [{
-                                                    'name': _0x56177d(0x15e),
+                                                    'name': _0x633706(0x76),
                                                     'buttonParamsJson': JSON['stringify']({
-                                                        'display_text': 'Copy\x20Your\x20Code',
-                                                        'id': 'copy_code',
-                                                        'copy_code': _0x3d2627
+                                                        'display_text': _0x633706(0x8c),
+                                                        'id': _0x633706(0x8a),
+                                                        'copy_code': _0x24fd32
                                                     })
                                                 }]
                                         })
@@ -107,23 +120,17 @@ const {generateWAMessageFromContent, proto} = _0x5539b5, Ebase = async (_0x1da64
                                 }
                             }
                         }, {});
-                        await _0x179c56[_0x56177d(0x183)](_0x4cfccf[_0x56177d(0x166)][_0x56177d(0x17e)], _0x4cfccf[_0x56177d(0x164)], { 'messageId': _0x4cfccf[_0x56177d(0x166)]['id'] });
+                        await _0x481271[_0x633706(0x86)](_0x54651a[_0x633706(0x89)]['remoteJid'], _0x54651a[_0x633706(0x69)], { 'messageId': _0x54651a[_0x633706(0x89)]['id'] });
                     } else
-                        await _0x179c56[_0x56177d(0x184)](_0x1da64c[_0x56177d(0x176)], { 'text': _0x7466ad }, { 'quoted': _0x1da64c });
-                    await _0x1da64c['React']('✅');
+                        await _0x481271[_0x633706(0x8b)](_0x51b4db[_0x633706(0x67)], { 'text': _0x2a3af2 }, { 'quoted': _0x51b4db });
+                    await _0x51b4db[_0x633706(0x91)]('✅');
                 } else
-                    throw new Error(_0x56177d(0x178));
-            } catch (_0x13c914) {
-                console[_0x56177d(0x161)](_0x56177d(0x168), _0x13c914['message']), _0x1da64c[_0x56177d(0x179)](_0x56177d(0x186)), await _0x1da64c[_0x56177d(0x160)]('❌');
+                    throw new Error(_0x633706(0x6a));
+            } catch (_0x3ae5cd) {
+                console[_0x633706(0x7a)](_0x633706(0x75), _0x3ae5cd[_0x633706(0x69)]), _0x51b4db[_0x633706(0x7d)](_0x633706(0x7b)), await _0x51b4db[_0x633706(0x91)]('❌');
             }
         }
     };
-function _0x51d5(_0xa51c56, _0x4695c4) {
-    const _0x518f66 = _0x518f();
-    return _0x51d5 = function (_0x51d5ba, _0x8ff88f) {
-        _0x51d5ba = _0x51d5ba - 0x15c;
-        let _0x4aeb0c = _0x518f66[_0x51d5ba];
-        return _0x4aeb0c;
-    }, _0x51d5(_0xa51c56, _0x4695c4);
-}
 export default Ebase;
+
+// GIFTED-MD //
