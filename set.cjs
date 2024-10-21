4298581659d50d2781505a052e69bc819df6656e
Gifted Maurice <166598671+mouricedevs@users.noreply.github.com>
2024-10-21 05:20:13 +0300

    @mouricedevs 𝗚𝗜𝗙𝗧𝗘𝗗-𝗠𝗗 𝗩𝟱

--- set.cjs
+++ set.cjs
@@ -18,7 +18,7 @@ const config = {
   
   AUTO_REJECT_CALLS: process.env.AUTO_REJECT_CALLS !== undefined ? process.env.AUTO_REJECT_CALLS === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
-  MODE: process.env.MODE || "private",
+  MODE: process.env.MODE || "public",
   OWNER_NAME: process.env.OWNER_NAME || "ɢɪғᴛᴇᴅ ᴛᴇᴄʜ",
   OWNER_NUMBER: process.env.OWNER_NUMBER || "254762016957",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyAkHGe6qWMujR2BjM468vQbY2RIbp0VXyc",
