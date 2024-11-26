9d91bb9c54e314fc952950853d0f8e8fd6c81542
THISAGA-MD <alexthisaga@gmail.com>
2024-11-26 19:44:06 +0530

    Update set.cjs

--- set.cjs
+++ set.cjs
@@ -2,7 +2,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "",
+  SESSION_ID: process.env.SESSION_ID || "Gifted~S3whhCQZ#686K0O-6_OrEpfW0pc1IavctzAO-LQKsL0nVMHO_Zqg",
   PREFIX: process.env.PREFIX || '.',
   AUTOLIKE_EMOJI: process.env.AUTOLIKE_EMOJI || '💚', // For liking whatsapp status updates(stories)
   AUTO_READ_STATUS: process.env.AUTO_READ_STATUS !== undefined ? process.env.AUTO_READ_STATUS === 'true' : true,
