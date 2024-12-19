b5bc6addcc2ed83dd6f00df720bb5fefa5e3d379
Gifted Maurice <166598671+mouricedevs@users.noreply.github.com>
2024-12-19 06:36:43 +0300

    @mouricedevs 𝗚𝗜𝗙𝗧𝗘𝗗-𝗠𝗗 𝗩𝟱

--- gift/index.js
+++
@@ -1,9 +0,0 @@
-const config = require('../set'),
-      GroupUpdate = require('./giftedgroupevents'),
-      { giftedProcessImage } = require('./giftedhd'),
-      { giftedmd, downloadMediaMessage } = require('./giftedmedia'),
-      { emojis, doReact } = require('./giftedareact'),
-      { cmd, commands, events } = require('./giftedcmds'),
-      { GiftedAnticall, GiftedFancy, Giftedttstalk, GiftedApkDl, giftedTempmail, getBuffer, giftedUrls,  getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, toAudio, toPTT, eBasef, dBasef, eBinary, dBinary, toVideo, ffmpeg, giftedPlugins, fetchJson } = require('./giftedfunction');
-
-module.exports = { GiftedAnticall, GroupUpdate, GiftedFancy, Giftedttstalk, GiftedApkDl, giftedTempmail, giftedProcessImage, getBuffer, giftedUrls, giftedPlugins, getGroupAdmins, getRandom, h2k, isUrl, Json, config, runtime, sleep, toAudio, toPTT, eBasef, dBasef, eBinary, dBinary, toVideo, ffmpeg, fetchJson, emojis, doReact, giftedmd, downloadMediaMessage, cmd, commands, events };
