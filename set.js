d27d297d7511c04e77cd2d6ebbaa2e16d1eaf3b3
Lunatech2 <lunashi46@gmail.com>
2024-12-29 18:02:52 +0300

    Update set.js

--- set.js
+++ set.js
@@ -4,7 +4,7 @@ const fs = require('fs'),
       dotenv = fs.existsSync('config.env') ? require('dotenv').config({ path: '/.env' }) : undefined,
       convertToBool = (text, fault = 'true') => text === fault;
 
-global.caption = "*©𝟐𝟎24 DECLAH 𝐌𝐃 𝐕𝟓*"; // Input Yours custom...(Maintain font for Flow)
+global.caption = "*©𝟐𝟎24 LUNATIC 𝐌𝐃 𝐕𝟓*"; // Input Yours custom...(Maintain font for Flow)
 global.api = "https://api.giftedtech.my.id/api"; // DO NOT Change this...
 global.session = "https://pair1.giftedtech.my.id"; // DO NOT Change this....
 global.footer = "*ᴘᴏᴡᴇʀᴇᴅ ʙʏ LUNA ᴛᴇᴄʜ*"; // Input Yours custom...(Maintain font for Flow)
@@ -21,12 +21,12 @@ AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 AUTO_LIKE_STATUS: process.env.AUTO_LIKE_STATUS || "true",
 AUTO_LIKE_EMOJI: process.env.AUTO_LIKE_EMOJI || "💜", //Input Yours custom...
 AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "false",
-STATUS_REPLY_MSG: process.env.STATUS_REPLY_MSG || "✅️ Status Viewed by Declah-Md!", // // Input Yours custom...
+STATUS_REPLY_MSG: process.env.STATUS_REPLY_MSG || "✅️ Status Viewed by LUNATIC-MD!", // // Input Yours custom...
 MODE: process.env.MODE || "public", // Put private or public or inbox or groups
 OWNER_NUMBER: process.env.OWNER_NUMBER || "14028169012", // Only 1 owner Number Here, others Add to sudo numbers...
 OWNER_NAME: process.env.OWNER_NAME || "LUNA 𝐓𝐄𝐂𝐇", // Input Yours custom...(Maintain font for Flow)
 PACK_AUTHOR: process.env.PACK_AUTHOR || "LUNA 𝐓𝐄𝐂𝐇", // Added // Input Yours custom...
-PACK_NAME: process.env.PACK_NAME || "DECLAH-𝐌𝐃💜", // Added // Input Yours custom...
+PACK_NAME: process.env.PACK_NAME || "LUNATIC-𝐌𝐃💜", // Added // Input Yours custom...
 PREFIX: process.env.PREFIX || ".",
 API_KEY: process.env.API_KEY || "declah-md", // Replace with your unlimited api key
 // ANTIDELETE: process.env.ANTIDELETE || "false", // Still under development
@@ -34,19 +34,19 @@ ANTILINK: process.env.ANTILINK || "false", //  Enter true to kick automatically 
 ANTICALL: process.env.ANTICALL || "false",
 ANTIBAD: process.env.ANTIBAD || "false",
 BAD_WORDS: process.env.BAD_WORDS || "fuck, pussy, anus, idiot", // Add Yours Separated by Comma(will be deleted if ANTIBAD is set to true)
-ANTICALL_MSG: process.env.ANTICALL_MSG || "*_📞 Auto Call Reject Mode Activated by Declah-Md. 📵 No Calls Allowed Dude!_*",
+ANTICALL_MSG: process.env.ANTICALL_MSG || "*_📞 Auto Call Reject Mode Activated by LUNAIC-Md. 📵 No Calls Allowed Dude!_*",
 AUTO_REACT: process.env.AUTO_REACT || "false",
-BOT_NAME: process.env.BOT_NAME || "DECLAH-𝐌𝐃", // Input Yours custom...(Maintain font for Flow)
-BOT_PIC: process.env.BOT_PIC || "https://i.imgur.com/kXHLkVN.mp4", // You can Replace with yours...
+BOT_NAME: process.env.BOT_NAME || "LUNATIC-𝐌𝐃", // Input Yours custom...(Maintain font for Flow)
+BOT_PIC: process.env.BOT_PIC || "https://files.catbox.moe/0gslwm.jpg", // You can Replace with yours...
 AUTO_AUDIO: process.env.AUTO_AUDIO || "false",
 AUTO_BIO: process.env.AUTO_BIO || "false",
-AUTO_BIO_QUOTE: process.env.AUTO_BIO_QUOTE || "Declah MD creates things that creates other things",
+AUTO_BIO_QUOTE: process.env.AUTO_BIO_QUOTE || "LUNATIC MD creates things that creates other things",
 CHAT_BOT: process.env.CHAT_BOT || "false", // Put value to true to enablle for all chats only or inbox to ebanle in pm chats only or groups to enable in groups only else false
 WELCOME: process.env.WELCOME || "false",
 GOODBYE: process.env.GOODBYE || "false",    
 AUTO_READ_MESSAGES: process.env.AUTO_READ_MESSAGES || "false", // Enter value to true for blueticking all messages, or commands for blueticking only commands else false
 AUTO_BLOCK: process.env.AUTO_BLOCK || "212,79", // Add Multiple Country Codes Separated by Comma...
-PRESENCE: process.env.PRESENCE || "online", // Choose one: typing, recording, online, null
+PRESENCE: process.env.PRESENCE || "typing", // Choose one: typing, recording, online, null
 TIME_ZONE: process.env.TIME_ZONE || "Africa/Nairobi", // Enter yours else leave blank if not sure
 };
 
